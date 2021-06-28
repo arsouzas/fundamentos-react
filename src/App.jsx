eee952e57654c2978afca85254afc8124adc585a
arsouzas <anderson.silva@multiplica.local>
2021-06-28 15:12:18 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Primeiro from './components/Primeiro'
+import './App.css'
+import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
+import Card from './components/layout/Card'
+import Repeticao from './components/Repeticao'
+import Condicional from './components/Condicional'
+import CondicionalComIf from './components/CondicionalComIf'
+
+export default (props) => (
+    <div className="App">
+       
+
+       <Card titulo="#06 - Condicional v2">
+            <CondicionalComIf numero={10}></CondicionalComIf>
+        </Card>
+       <Card titulo="#05 - Condicional v1">
+            <Condicional numero={15}></Condicional>
+        </Card>
+        <Card titulo="#04 - Repetição">
+            <Repeticao></Repeticao>
+        </Card>
+
+        <Card titulo="#03 - Componente com Filhos">
+            <ComFilhos>
+                <ul>
+                    <li>Ana</li>
+                    <li>Bia</li>
+                    <li>Daniel</li>
+                </ul>
+            </ComFilhos>
+        </Card>
+        <Card titulo="#02 -Componente com Parametro">
+            <ComParametro titulo="Esse é o titulo" 
+            subtitulo="Esse é o subtitulo"  />
+        </Card>
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro />
+        </Card>
+    
+    </div>
+
+
+
+);
